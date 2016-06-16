2736ac3c027a4755299988b6fe8de87a5e2891d6
Kimi Päivärinta <mail@kimin.info>
2016-06-16 23:15:56 +0300

    New class for parsing significant information from JSON

---
+++ src/jsonparser.py
@@ -0,0 +1,43 @@
+'''
+Created on 14.6.2016
+
+@author: Kalmis
+'''
+
+from dateutil.parser import *
+from dateutil.relativedelta import *
+import datetime
+
+
+class JSONParser(object):
+    '''
+    Class for Parsing JSON. Methods of this class can be used to obtain e.g. Amica's menus
+    '''
+
+    def getAmicaDays(self,json):
+        MenusForDays = json['MenusForDays']
+        dates = []
+        for day in MenusForDays:
+            dates.append(parse(day['Date']))
+        return dates
+
+
+    def getAmicaMenuForDay(self,json,date):
+        for day in json['MenusForDays']:
+            MenuDate = parse(day['Date'])
+            if date.date() == MenuDate.date():
+                return day
+        return 0
+
+    def getAmicaSetMenu(self,MenuForDay,menuName):
+        for SetMenu in MenuForDay['SetMenus']:
+            if SetMenu['Name'].lower() == menuName.lower():
+                return SetMenu
+        return 0
+
+
+    def getAmicaSetMenuForDate(self,json,date,SetMenuName):
+
+        MenuForDay=self.getAmicaMenuForDay(json,date)
+        SetMenu = self.getAmicaSetMenu(MenuForDay,SetMenuName)
+        return SetMenu['Components']
