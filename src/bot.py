a7e693850c5141d2ef3097324491ff80f18a340c
Kimi Päivärinta <mail@kimin.info>
2016-06-18 16:42:25 +0300

    Using new function to reply with full menu, not only lunch and soap

--- src/bot.py
+++ src/bot.py
@@ -9,25 +9,22 @@ Created on 14.6.2016
 
 import telepot
 import requests
-import json
-import configparser
-import pprint
 import datetime
 from jsonparser import JSONParser
 
 
-
 class YourBot(telepot.Bot):
+
     def __init__(self, *args, **kwargs):
         super(YourBot, self).__init__(*args, **kwargs)
         self._answerer = telepot.helper.Answerer(self)
         self._message_with_inline_keyboard = None
         self.JSONParser = JSONParser()
 
-    def downloadMenus(self,config):
+    def downloadMenus(self, config):
         self.menus = {}
         for option in config.options('RESTAURANTS'):
-            r = requests.get(config.get('RESTAURANTS',option))
+            r = requests.get(config.get('RESTAURANTS', option))
             if r.status_code == 200:
                 self.menus[option] = r.json()
 
@@ -37,14 +34,14 @@ class YourBot(telepot.Bot):
 
         if content_type != 'text':
             return
-
         command = msg['text'].split()[0][1:].lower()
         msg_id = msg['message_id']
 
         if command in self.menus:
-            reply = ""
-            lunch = "Lounas: \n" + self.JSONParser.getAmicaSetMenuForDate(self.menus[command],datetime.datetime.now(),"Lounas")
-            vegLunch = "Kasvislounas: \n" + self.JSONParser.getAmicaSetMenuForDate(self.menus[command],datetime.datetime.now(),"Kasvislounas")
-            vegSoap = "Kasviskeitto: \n" + self.JSONParser.getAmicaSetMenuForDate(self.menus[command],datetime.datetime.now(),"Kasviskeitto")
-            reply = lunch + "\n" + vegLunch + "\n" + vegSoap
+            today = datetime.datetime.now()
+            fullMenu = self.JSONParser.getAmicaFullMenuForDate(self.menus[command], today)
+            if len(fullMenu) > 0:
+                reply = fullMenu
+            else:
+                reply = "Ei listaa tälle päivälle"
             self.sendMessage(chat_id, reply, reply_to_message_id=msg_id)
