c079e7289c86586bb94b364b5369f1671860c0cd
Kimi Päivärinta <mail@kimin.info>
2016-06-18 16:41:37 +0300

    Added new functions to get whole menu of the day

--- src/jsonparser.py
+++ src/jsonparser.py
@@ -8,41 +8,53 @@ Created on 14.6.2016
 
 from dateutil.parser import *
 from dateutil.relativedelta import *
-import datetime
 
 
 class JSONParser(object):
     '''
-    Class for Parsing JSON. Methods of this class can be used to obtain e.g. Amica's menus
+    Class for Parsing JSON. Methods of this class can be used to
+    obtain e.g. Amica's menus
     '''
 
-    def getAmicaDays(self,json):
+    def getAmicaDays(self, json):
         MenusForDays = json['MenusForDays']
         dates = []
         for day in MenusForDays:
             dates.append(parse(day['Date']))
         return dates
 
-
-    def getAmicaMenuForDay(self,json,date):
+    def getAmicaMenuForDay(self, json, date):
         for day in json['MenusForDays']:
             MenuDate = parse(day['Date'])
             if date.date() == MenuDate.date():
                 return day
         return 0
 
-    def getAmicaSetMenu(self,MenuForDay,menuName):
+    def getAmicaMenuNamesForDate(self, json, date):
+        names = []
+        MenuForDay = self.getAmicaMenuForDay(json, date)
+        for SetMenu in MenuForDay['SetMenus']:
+            names.append(SetMenu['Name'])
+        return names
+
+    def getAmicaSetMenu(self, MenuForDay, menuName):
         for SetMenu in MenuForDay['SetMenus']:
             if SetMenu['Name'].lower() == menuName.lower():
                 return SetMenu
         return 0
 
+    def getAmicaSetMenuForDate(self, json, date, SetMenuName):
 
-    def getAmicaSetMenuForDate(self,json,date,SetMenuName):
-
-        MenuForDay=self.getAmicaMenuForDay(json,date)
-        SetMenu = self.getAmicaSetMenu(MenuForDay,SetMenuName)
+        MenuForDay = self.getAmicaMenuForDay(json, date)
+        SetMenu = self.getAmicaSetMenu(MenuForDay, SetMenuName)
         setMenuString = ""
         for food in SetMenu['Components']:
-            setMenuString+=food+'\n'
+            setMenuString += food + '\n'
         return setMenuString
+
+    def getAmicaFullMenuForDate(self, json, date):
+        setMenuNames = self.getAmicaMenuNamesForDate(json, date)
+        returnText = ""
+        for name in setMenuNames:
+            returnText += name + '\n' + self.getAmicaSetMenu(json, date, name)
+        return returnText
