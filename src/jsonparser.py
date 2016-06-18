c07d7a28422914a43ba81d10e1e7fa3495eb6e93
Kimi Päivärinta <mail@kimin.info>
2016-06-19 00:31:23 +0300

    Renamed JSONParser to menuParser and made all functions @staticmethods … Renamed JSONParser class to menuParser and made all functions @staticmethods no data is stored in objects

--- src/jsonparser.py
+++ src/jsonparser.py
@@ -6,55 +6,64 @@ Created on 14.6.2016
 @author: Kalmis
 '''
 
-from dateutil.parser import *
-from dateutil.relativedelta import *
+from dateutil.parser import parse
 
 
-class JSONParser(object):
+class menuParser(object):
     '''
-    Class for Parsing JSON. Methods of this class can be used to
+    Class for Parsing dict. Methods of this class can be used to
     obtain e.g. Amica's menus
     '''
-
-    def getAmicaDays(self, json):
-        MenusForDays = json['MenusForDays']
+    @staticmethod
+    def getAmicaDays(dict):
+        ''' From given Amica menu dict, returns dates for given menus'''
+        MenusForDays = dict['MenusForDays']
         dates = []
         for day in MenusForDays:
             dates.append(parse(day['Date']))
         return dates
 
-    def getAmicaMenuForDay(self, json, date):
-        for day in json['MenusForDays']:
+    @staticmethod
+    def getAmicaMenuForDay(dict, date):
+        '''Returns menu dict for given day'''
+        for day in dict['MenusForDays']:
             MenuDate = parse(day['Date'])
             if date.date() == MenuDate.date():
                 return day
         return 0
 
-    def getAmicaMenuNamesForDate(self, json, date):
+    @staticmethod
+    def getAmicaMenuNamesForDate(dict, date):
+        ''' From given Amica menu dict, returns menu names for given day'''
         names = []
-        MenuForDay = self.getAmicaMenuForDay(json, date)
+        MenuForDay = menuParser.getAmicaMenuForDay(dict, date)
         for SetMenu in MenuForDay['SetMenus']:
             names.append(SetMenu['Name'])
         return names
 
-    def getAmicaSetMenu(self, MenuForDay, menuName):
+    @staticmethod
+    def getAmicaSetMenu(MenuForDay, menuName):
+        '''Get food of given menu'''
         for SetMenu in MenuForDay['SetMenus']:
             if SetMenu['Name'].lower() == menuName.lower():
                 return SetMenu
         return 0
 
-    def getAmicaSetMenuForDate(self, json, date, SetMenuName):
-
-        MenuForDay = self.getAmicaMenuForDay(json, date)
-        SetMenu = self.getAmicaSetMenu(MenuForDay, SetMenuName)
+    @staticmethod
+    def getAmicaSetMenuForDate(dict, date, SetMenuName):
+        '''Get food for given day and menu name'''
+        MenuForDay = menuParser.getAmicaMenuForDay(dict, date)
+        SetMenu = menuParser.getAmicaSetMenu(MenuForDay, SetMenuName)
         setMenuString = ""
         for food in SetMenu['Components']:
             setMenuString += food + '\n'
         return setMenuString
 
-    def getAmicaFullMenuForDate(self, json, date):
-        setMenuNames = self.getAmicaMenuNamesForDate(json, date)
+    @staticmethod
+    def getAmicaFullMenuForDate(dict, date):
+        '''Get all foods of given day'''
+        setMenuNames = menuParser.getAmicaMenuNamesForDate(dict, date)
         returnText = ""
         for name in setMenuNames:
-            returnText += name + '\n' + self.getAmicaSetMenu(json, date, name)
+            returnText += name + '\n' + menuParser.getAmicaSetMenu(dict, date, name)
         return returnText
