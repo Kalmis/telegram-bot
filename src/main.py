4fee36d524930f755922d4a2467128172c661a1a
Kimi Päivärinta <mail@kimin.info>
2020-05-01 17:47:35 +0300

    Starting the telegram bot project from beginning with new featrues

--- src/main.py
+++ src/main.py
@@ -1,50 +1,40 @@
-# -*- coding: utf-8 -*-
-
-'''
-Created on 17.5.2016
-
-@author: Kalmis
-'''
-
-import configparser
-import time
-from bot import YourBot
-import schedule
-
-conf = 'bot.conf'
-tokenconf = 'tokens.conf'
+import os
+import logging
+import glob
+import random
+from telegram.ext import Updater, CommandHandler
 
 
-class Main(object):
-    '''
-    Main program that uses the Bot class
-    '''
-    def __init__(self):
-        tokens = configparser.ConfigParser()
-        tokens.read(tokenconf)
-        TOKEN = tokens['TELEGRAM']['Token']
+IMAGES_PATH = '/docs/images/'
 
-        # Create bot object, download menus set in config and start loop
-        self.bot = YourBot(TOKEN)
-        self.bot.readConfigs(conf)
-        self.bot.initGeopyGoogle(tokens['GOOGLE']['Token'])
-        self.downloadMenus()
-        self.bot.message_loop(self.bot.on_message)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger("telegram").setLevel(logging.WARNING)
+logger = logging.getLogger('telegram-bot.' + __name__)
+logger.setLevel(logging.INFO)
 
-        # Download menus every 6 hours to keep them up to date
-        schedule.every(6).hours.do(self.downloadMenus)
-        print('Started')
+API_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-        while 1:
-            schedule.run_pending()
-            time.sleep(10)
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-    def downloadMenus(self):
-        self.bot.downloadSodexoMenus()
-        self.bot.downloadTaffaMenu()
-        self.bot.downloadAmicaMenus()
-        self.bot.downloadHYYMenus()
-        self.bot.readSubwayMenu()
 
-if __name__ == '__main__':
-    Main()
+def random_photo(update, context):
+
+    photos = []
+    for extension in ['*.JPG', '*.JPEG']:
+        photos += glob.glob(IMAGES_PATH + extension)
+    photo = random.choice(photos)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(photo, 'rb'))
+
+
+if __name__ == "__main__":
+    logger.info("Starting")
+    updater = Updater(token=API_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    kusti_handler = CommandHandler('kusti', random_photo)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(kusti_handler)
+    logger.info("Starting polling")
+    updater.start_polling()
